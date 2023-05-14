d7eda5d30ac5b2c2fba0f8c230668c95ae7e0c33
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-05-14 17:28:48 +0200

    Added working SystemTray

--- JUtils/App.xaml.cs
+++ JUtils/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Forms = System.Windows.Forms;
 
 namespace JUtils
 {
@@ -13,5 +14,51 @@ namespace JUtils
     /// </summary>
     public partial class App : Application
     {
+        private readonly Forms.NotifyIcon _notifyIcon;
+
+        public App()
+        {
+            _notifyIcon = new Forms.NotifyIcon();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            //MainWindow.Show();
+
+            setupSystemTray();
+
+            base.OnStartup(e);
+        }
+
+        private void setupSystemTray()
+        {
+            _notifyIcon.Icon = new System.Drawing.Icon("Resources/SystemTrayIcon.ico");
+            _notifyIcon.Text = "JUtils";
+            _notifyIcon.MouseClick += _notifyIcon_MouseClick;
+            _notifyIcon.ContextMenuStrip = new Forms.ContextMenuStrip();
+            _notifyIcon.ContextMenuStrip.Items.Add("Open", null, (object? sender, EventArgs e) => openMainWindow());
+            _notifyIcon.ContextMenuStrip.Items.Add("Exit", null, (object? sender, EventArgs e) => Shutdown());
+            _notifyIcon.Visible = true;
+        }
+
+        private void openMainWindow()
+        {
+            MainWindow.WindowState = WindowState.Normal;
+            MainWindow.Show();
+            MainWindow.Activate();
+        }
+
+        private void _notifyIcon_MouseClick(object? sender, Forms.MouseEventArgs e)
+        {
+            if (e.Button == Forms.MouseButtons.Right) return;
+            openMainWindow();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _notifyIcon.Dispose();
+            base.OnExit(e);
+        }
     }
 }
--- JUtils/MainWindow.xaml.cs
+++ JUtils/MainWindow.xaml.cs
@@ -41,5 +41,26 @@ namespace JUtils
         {
             controller.ShutdownHotkeySystemHook();
         }
+
+        // Minimize to system tray when application is minimized.
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (IsVisible && WindowState == WindowState.Minimized) this.Hide();
+
+            base.OnStateChanged(e);
+        }
+
+        // Minimize to system tray when application is closed.
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            // setting cancel to true will cancel the close request
+            // so the application is not closed
+            e.Cancel = true;
+
+            this.Hide();
+            WindowState = WindowState.Minimized;
+
+            base.OnClosing(e);
+        }
     }
 }
