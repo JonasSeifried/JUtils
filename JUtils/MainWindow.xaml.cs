bd6390a43edb1be3f9b4f59e53f640a8a1610e35
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2023-05-16 23:08:27 +0200

    workflow bugfix

--- JUtils/MainWindow.xaml.cs
+++ JUtils/MainWindow.xaml.cs
@@ -1,14 +1,10 @@
 ﻿
 using System;
-
 using System.Windows;
 using System.Windows.Media;
 
 namespace JUtils
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
         public MainWindow()
